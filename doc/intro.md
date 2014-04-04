63887679500a9f78580eba5533f45800900de6fa
Sebastien Soudan <sebastien@lyatiss.com>
2014-04-04 16:27:58 +0200

    [initial commit] added new files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ai2048
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ai2048 "0.1.0-SNAPSHOT"
+  :description "ai2048: write description"
+  :url "http://example.com/ai2048"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ai2048.core)
